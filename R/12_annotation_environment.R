0ccfc8d741b18db1e77871d0c48439d27b4d940a
Jens van Erp <61182816+JensvanErp@users.noreply.github.com>
2023-03-28 15:34:39 +0200

    This is a major overhaul (sorry, ill try to break it up next time) Tested with the era5 and tracks data of birdar
    - Changed function and variable names to reflect any environmental variable can be added (removed "weather" and "era" mentions)
    - Changed variables vector so it now needs to include the timestamp name of the env dataset as first entry. This adds flexibility on what name is needed for the timestamp in the env dataset (now grabs this variable name if looking for timestamp of env data throughout).
    - Added checks for data.table for both datasets
    - Added if-statement to translation of timestamp to posixct
    - Nearest_hour -> nearest_time to reflect flexibility of unit, changed sequence as normally duration should be used given the user used the metrics function, corrected an error in the nearest_time calculation (time diff needed to be multiplied by 0.5)
    - Simplified merge, also required as old code didn't allow for flexible time variable name in env data.
    - Changed help-page according to changes above

--- R/12_annotation_environment.R
+++ R/12_annotation_environment.R
@@ -1,16 +1,16 @@
 #' @importFrom data.table fread
 #' @importFrom lubridate round_date ymd_hms
 #'
-#' @title annotation
-#' @name annotation
-#' @description annotate the trajectory with environmental data
+#' @title add_env
+#' @name add_env
+#' @description annotate a track dataset with environmental data
 #'
-#' @param x sf object with tracks
-#' @param era5 path/file/API ERA5 dataset with annotation variables,
-#' @param variables vector with ERA5 variables to be annotated
+#' @param x data.frame or data.table object with tracks
+#' @param env data.frame or data.table with environmental data, or path to dataset
+#' @param variables character vector with environmental variables to be annotated. the first index should always be the time
 #' @param unit the annotation unit (hours)
 #'
-#' @return returns the tracks with era5 annotation
+#' @return returns the track dataset with environmental variables annotated
 #'
 #' @details
 #'
@@ -19,39 +19,47 @@
 #' @examples
 #'
 #'
-weather <- function(x=tracks_step1,
-                    era5=era,
-                    variables=c("U10m","V10m"),
-                    unit="hours"
-                    ){
-  var <- variables
-  ## Load in the wind data
-  # if PATH name
-  if(is.character(era5)){ # TODO double check if correct
-    era5 <- fread(era5)
+add_env <- function(x=tracks_step1,
+                    env=era,
+                    variables=c("date_time","U10m","V10m"),
+                    unit="hours"){
+  ## Track data
+  ## if not data.table
+  if (class(x)[1]!="data.table") {
+    x <- as.data.table(x)
   }
-  # if API
+
+  ## Environmental data
+  ## if path
+  if(is.character(env)){ # TODO double check if correct
+    env <- fread(env)
+  }
+  ## if not data.table
+  if (class(env)[1] != "data.table") {
+    env <- as.data.table(env)
+  }
+  ## if API
   # TODO API statement might also be character input - To Be Checked
-
-  ## Make date_time into POSIXct:
-  era5[,date_time:=ymd_hms(date_time, tz="UTC")]
-
-  # use start and end time
-  if(!'duration' %in% colnames(x)){
-  x[,nearest_hour:=round_date((timestamp_start + (timestamp_end - timestamp_start)),unit=unit)]
-  } else {
-  ## To add weather conditions for each track, calculate the rounded hour of the half-way point of each track first
-  x[,nearest_hour:=round_date((timestamp_start+0.5*duration),unit=unit)]
+  ## if time not POSIXct
+  if (class(env[,get(variables[1])])[1] != "POSIXct"){
+    env[,get(variables[1]):=ymd_hms(get(variables[1]), tz="UTC")]
   }
-  ## Create exception for last hour in the dataset is the weather data holds the same
-  x[!(nearest_hour %in% era5$date_time), nearest_hour:=era5$date_time[nrow(era5)]]
 
-  ## Add wind conditions (in this case U and V component at 10m ASL) to the tracks based on their timestamp
-  var <- c("date_time",var)
-  x <- x[era5[, ..var], on=.(nearest_hour=date_time)]
-  ## Remove duplicate rows caused by hours without tracks (but with wind data available)
-  x <- x[!is.na(id),]
-  x[,"nearest_hour":=NULL]
+  ## Get nearest time of each track, based on timestamp near mid point of track
+  # Use duration if available
+  if('duration' %in% colnames(x)){
+    x[,nearest_time:=round_date((timestamp_start + 0.5*duration),unit=unit)]
+  } else { ## Otherwise use start and end time
+    x[,nearest_time:=round_date((timestamp_start + 0.5*(timestamp_end - timestamp_start)),unit=unit)]
+  }
+  ## Create exception for last timestamp in the dataset if the env data doesn't hold it
+  ## e.g., if time rounds up to next day while env data is available until last hour of previous day
+  x[!(nearest_time %in% env[,get(variables[1])]), nearest_time:=env[,get(variables[1])[nrow(env)]]]
+
+  ## Annotate environmental conditions to the tracks based on overlap in time
+  x <- merge(x,env[, ..variables],by.x="nearest_time",by.y=variables[1])
+  ## Remove nearest time
+  x[,"nearest_time":=NULL]
 
   return(x)
 }
